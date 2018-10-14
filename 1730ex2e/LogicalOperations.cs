666034f3cc5c855a3fd4a755e78bc937a4519dc5
mswanson-mscst <mswanson@southeastmn.edu>
2018-10-14 17:13:07 -0500

    Initial commit

---
+++ 1730ex2e/LogicalOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1730ex2e
+{
+    public class LogicalOperations
+    {
+        public static bool q01(decimal subtotal)
+        {
+            return false;
+        }
+        public static bool q02(int timeInService)
+        {
+            return false;
+        }
+        public static bool q03(bool isValid, int years, int counter)
+        {
+            return false;
+        }
+        public static bool q04(bool isValid, int years, int counter)
+        {
+            return false;
+        }
+        public static bool q05(bool isValid, int years, int counter)
+        {
+            return false;
+        }
+        public static bool q06(bool isValid, int years, int counter)
+        {
+            return false;
+        }
+        public static bool q07(DateTime startDate, DateTime expirationDate, DateTime date, bool isValid)
+        {
+            return false;
+        }
+        public static bool q08(decimal thisYTD, decimal lastYTD, string empType, int startYear, int currentYear)
+        {
+            return false;
+        }
+        public static bool q09(int counter, int years)
+        {
+            return false;
+        }
+    }
+}
