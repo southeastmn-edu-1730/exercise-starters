8f2f11e2faf6df3bc9076f15aac9e31a66506ad0
mswanson-mscst <mswanson@southeastmn.edu>
2018-10-29 16:16:19 -0500

    Added ex2f unit tests

---
+++ 1730ex2f/Ex2fCalculations.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1730ex2f
+{
+    public class Ex2fCalculations
+    {
+        public static string Calc01(string input)
+        {
+            // #1: if
+            return "";
+        }
+
+        public static string Calc02(string input)
+        {
+            // #2: if {block}
+            return "";
+        }
+
+        public static string Calc03(string input)
+        {
+            // #3: if else
+            return "";
+        }
+
+        public static string Calc04(string input)
+        {
+            // #4 if else if
+            return "";
+        }
+
+        public static string Calc05(string input)
+        {
+            // #5 Better range test
+            return "";
+        }
+
+        public static string Calc06(string inputA, string inputB)
+        {
+            // #6 Nested if
+            return "";
+        }
+
+        public static string Calc07(string input)
+        {
+            // #7 Validate input: non-empty string
+            return "";
+        }
+
+        public static string Calc08(string inputA, string inputB)
+        {
+            // #8 Validate input, calculate quantity * price, shipping
+            return "";
+        }
+
+        public static string Calc09(string inputA, string inputB)
+        {
+            // #9 Validate input, calculate difference * rate
+            return "";
+        }
+
+        public static string Calc10(string inputA, string inputB)
+        {
+            // #10 Validate input, divide large num by small
+            //     Both numbers must be > 0
+            return "";
+        }
+    }
+}
