7683eb74ca6bc2df1d5ca0d9aaffcd523d103163
mswanson-mscst <mswanson@southeastmn.edu>
2018-11-01 11:29:22 -0500

    Added ex2g unit tests

---
+++ 1730ex2g/Ex2gCalculations.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1730ex2g
+{
+    public class Ex2gCalculations
+    {
+        public static string Switch01(string customerType)
+        {
+            // 1a) 'Switch' with no default
+            return "";
+        }
+        public static string If01(string customerType)
+        {
+            // 1b) Separate 'if' statements
+            return "";
+        }
+        public static string ElseIf01(string customerType)
+        {
+            // 1c) if elseif
+            return "";
+        }
+        public static string NestedIfElse01(string customerType)
+        {
+            // 1d) Nested if-else
+            return "";
+        }
+        public static string SwitchDefault01(string customerType)
+        {
+            // 1e) 'Switch' with default
+            return "";
+        }
+        public static string IfDefault01(string customerType)
+        {
+            // 1f) Separate 'if' statements with default
+            return "";
+        }
+        public static string ElseIfDefault01(string customerType)
+        {
+            // 1g) if elseif with default
+            return "";
+        }
+        public static string NestedIfElseDefault01(string customerType)
+        {
+            // 1h) Nested if-else with default
+            return "";
+        }
+        public static string Switch02(string customerType)
+        {
+            // 2a) 'Switch' with no default
+            return "";
+        }
+        public static string If02(string customerType)
+        {
+            // 2b) Separate 'if' statements
+            return "";
+        }
+        public static string ElseIf02(string customerType)
+        {
+            // 2c) if elseif
+            return "";
+        }
+        public static string NestedIfElse02(string customerType)
+        {
+            // 2d) Nested if-else
+            return "";
+        }
+    }
+}
