50c9a2512ac6aa53b3db3d8e953d7a6acd2e91b5
mswanson-mscst <mswanson@southeastmn.edu>
2018-11-07 11:38:30 -0600

    Added ex2h DateTime unit tests

---
+++ 1730ex2h/Ex2hCalculations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1730ex2h
+{
+    public class Ex2hCalculations
+    {
+        public static string DateCalc06(string strYear, string strMonth, string strDay)
+        {
+            return "Not implemented";
+        }
+        public static string DateCalc07(string strDate)
+        {
+            return "Not implemented";
+        }
+        public static string DateCalc08(string strDate)
+        {
+            return "Not implemented";
+        }
+        public static string DateCalc09(string strDateA, string strDateB)
+        {
+            return "Not implemented";
+        }
+        public static string DateCalc10(string strDateA, string strDateB)
+        {
+            return "Not implemented";
+        }
+    }
+}
