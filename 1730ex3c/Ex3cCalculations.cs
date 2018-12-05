585068ba9bbfc466b74dc1000a194c29c3b0c4c5
mswanson-mscst <mswanson@southeastmn.edu>
2018-12-05 17:20:35 -0600

    Added ex3c

---
+++ 1730ex3c/Ex3cCalculations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1730ex3c
+{
+    public class Ex3cCalculations
+    {
+        public static void Calc0()
+        {
+            string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        }
+
+        public static void Calc1()
+        {
+            string[] days = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
+            string[] hours = { "Closed", "10am - 6pm", "10am - 6pm", "10am - 6pm", "10am - 9pm", "10am - 6pm", "8am - 4pm" };
+        }
+
+        public static void Calc2()
+        {
+
+        }
+        public static void Calc3()
+        {
+            
+        }
+        public static void Calc5()
+        {
+            
+        }
+
+        public static void Calc6()
+        {
+            
+        }
+    }
+}
---
+++ 1730ex3cUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("1730ex3cUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1730ex3cUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b17785e3-434f-4ce8-977c-b74d6d8d6ffd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
