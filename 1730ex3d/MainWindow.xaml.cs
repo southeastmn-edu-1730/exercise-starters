42c73f7de2fa1112f76241fc33a529de51b37100
mswanson-mscst <mswanson@southeastmn.edu>
2018-12-13 12:19:21 -0600

    Added ex3d and unit tests

---
+++ 1730ex3d/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1730ex3d
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void searchValuesComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            valueCountTextBox.Text = "Test";
+        }
+
+        private void searchRangesComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int dashIndex = this.searchRangesComboBox.Text.IndexOf('-');
+            string strSearchMin = this.searchRangesComboBox.Text.Substring(0, dashIndex).Trim();
+            string strSearchMax = this.searchRangesComboBox.Text.Substring(dashIndex + 1).Trim();
+            rangeCountTextBox.Text = "Test";
+        }
+    }
+}
---
+++ 1730ex3dUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("1730ex3dUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1730ex3dUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59097a20-64fe-42fb-9c1d-b4c87da5596f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
